b8cf831a71e6339fb6078592ed8a3fe6bcb86d35
René <r.larch@inetcons.it>
2019-01-02 19:47:08 +0100

    fix setup.cmd

--- .setup.cmd
+++ .setup.cmd
@@ -1,5 +1,5 @@
 
 
 taskkill /IM "MouseTrap.exe" /F
-copy MouseTrap\bin\Debug\MouseTrap.exe C:\Tools\MouseTrap.exe
+copy MouseTrap\bin\MouseTrap.exe C:\Tools\MouseTrap.exe
 start C:\Tools\MouseTrap.exe
